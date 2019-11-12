7c4a135f39b4175b6221fa66e9dd7f8386bed122
付卫1 <fuwei1@dankegongyu.com>
2019-11-12 18:45:16 +0800

    delete not used file

--- apis/domain.go
+++
@@ -1,8 +0,0 @@
-package apis
-
-/*
-	DomainAdd	添加域名  POST /api/v1/domains
-	DomainDel	删除域名  DELETE /api/v1/domains/{id}
-	DomainList	域名列表  GET /api/v1/domains
-	Domain		单个域名  GET /api/v1/domains/{id}
-*/
